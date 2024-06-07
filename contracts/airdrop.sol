1cc4ff259be4add782c08339c9d848748b841f79
seokwns <seokjun0915@icloud.com>
2024-06-07 15:22:59 +0900

    feat: add claim status structure & view methods
    - 클레임 상태를 나타내는 구조체 추가
    - 에어드랍 시작 및 종료 여부를 반환하는 함수 추가

--- contracts/airdrop.sol
+++ contracts/airdrop.sol
@@ -8,8 +8,14 @@ contract AirDrop is Ownable {
     // This declares a state variable that would store the contract address
     IKIP7 public tokenInstance;
 
+    enum ClaimStatus {
+        CANNOT_CLAIM,
+        CLAIMABLE,
+        CLAIMED
+    }
+
     struct AirdropData {
-        bool claimable;
+        ClaimStatus status;
         uint256 amount;
     }
 
@@ -18,10 +24,9 @@ contract AirDrop is Ownable {
     mapping(address => AirdropData) private airdrops;
 
     event Claimed(address indexed receiver, uint256 amount);
+    event AirdropStartTimeUpdated(uint256 newStartTime);
+    event AirdropEndTimeUpdated(uint256 newEndTime);
 
-    /*
-    constructor function to set token address
-   */
     constructor(address _tokenAddress, uint256 _startTimestamp, uint256 _endTimestamp) {
         tokenInstance = IKIP7(_tokenAddress);
         startTimestamp = _startTimestamp;
@@ -32,27 +37,44 @@ contract AirDrop is Ownable {
         uint256 _startTimestamp
     ) public onlyOwner {
         startTimestamp = _startTimestamp;
+        emit AirdropStartTimeUpdated(_startTimestamp);
     }
     
     function updateEndTimestamp(
         uint256 _endTimestamp
     ) public onlyOwner {
         endTimestamp = _endTimestamp;
+        emit AirdropEndTimeUpdated(_endTimestamp);
+    }
+
+    function isStarted() public view returns (bool) {
+        return block.timestamp >= startTimestamp;
+    }
+
+    function isEnded() public view returns (bool) {
+        return block.timestamp > endTimestamp;
     }
 
     function claim() external {
         require(block.timestamp >= startTimestamp, "AIRDOP_NOT_STARTED");
         require(block.timestamp <= endTimestamp, "AIRDROP_ENDED");
-        require(airdrops[msg.sender].claimable, "CANNOT_CLAIM");
+        
+        ClaimStatus status = airdrops[msg.sender].status;
+
+        if (status == ClaimStatus.CANNOT_CLAIM) {
+            revert("CANNOT_CLAIM");
+        }
+        else if (status == ClaimStatus.CLAIMED) {
+            revert("ALREADY_CLAIMED");
+        }
 
         uint256 airdropAmount = airdrops[msg.sender].amount;
-        require(airdropAmount > 0, "ALREADY_CLAIMED");
-
         uint256 balance = tokenInstance.balanceOf(address(this));
         require(balance >= airdropAmount, "INSUFFICIENT_BALANCE");
 
         tokenInstance.transfer(msg.sender, airdropAmount);
         airdrops[msg.sender].amount = 0;
+        airdrops[msg.sender].status = ClaimStatus.CLAIMED;
 
         emit Claimed(msg.sender, airdropAmount);
     }
@@ -60,7 +82,7 @@ contract AirDrop is Ownable {
     function getAirdropData(
         address _address
     ) public view returns (AirdropData memory) {
-        require(airdrops[_address].claimable, "NO_DATA");
+        require(airdrops[_address].status != ClaimStatus.CANNOT_CLAIM, "NO_DATA");
         return airdrops[_address];
     }
 
@@ -68,11 +90,11 @@ contract AirDrop is Ownable {
         address _address,
         uint256 _amount
     ) public onlyOwner {
-        if (airdrops[_address].claimable) {
+        if (airdrops[_address].status == ClaimStatus.CLAIMABLE) {
             return;
         }
         airdrops[_address] = AirdropData({
-            claimable: true,
+            status: ClaimStatus.CLAIMABLE,
             amount: _amount
         });
     }
@@ -84,11 +106,11 @@ contract AirDrop is Ownable {
         require(_address.length == _amount.length, "INVALID_INPUT_DATA");
 
         for (uint256 i = 0; i < _address.length; i++) {
-            if (airdrops[_address[i]].claimable) {
+            if (airdrops[_address[i]].status == ClaimStatus.CLAIMABLE) {
                 continue;
             }
             airdrops[_address[i]] = AirdropData({
-                claimable: true,
+                status: ClaimStatus.CLAIMABLE,
                 amount: _amount[i]
             });
         }
@@ -104,7 +126,7 @@ contract AirDrop is Ownable {
     function deleteAirdropData(
         address _address
     ) public onlyOwner {
-        airdrops[_address].claimable = false;
+        airdrops[_address].status = ClaimStatus.CANNOT_CLAIM;
         airdrops[_address].amount = 0;
     }
 
