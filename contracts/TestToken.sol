cedfcea34acad646f6903b94620ac8acd1f073b2
seokwns <seokjun0915@icloud.com>
2024-11-04 16:09:39 +0900

    feat: update test token contract function
    - burn 을 위해 address(0) 로 transfer 할 수 있도록 수정

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TestToken is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
@@ -11,5 +10,13 @@ contract TestToken is ERC20 {
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        require(value > 0, "ERC20: transfer amount must be greater than zero");
+        require(balanceOf(msg.sender) >= value, "ERC20: transfer amount exceeds balance");
+
+        _update(msg.sender, to, value);
+        return true;
     }
 }
